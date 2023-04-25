d5b7d0fc93637d506785d0b0dd0e3a7228f600ce
Lucas <lucasreis64@gmail.com>
2023-04-25 03:28:08 -0300

    fixing

--- src/app/shared/types/types.ts
+++ src/app/shared/types/types.ts
@@ -1,14 +1,1 @@
-import { ThreeDots } from "react-loader-spinner";
-
-export const carregamento = (
-  <ThreeDots
-      height="80"
-      width="80"
-      radius="9"
-      color="white"
-      ariaLabel="three-dots-loading"
-      wrapperStyle={{}}
-      wrapperClassName=""
-      visible={true}
-  />
-);
+export {};
